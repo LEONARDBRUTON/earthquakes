3fe00221cde510e091ccc8704c5e420ec192b2a4
enigmalb <enigmalb@outlook.com>
2020-01-15 18:36:14 -0800

    fixed menu

--- lib/current_earthquakes/api_service.rb
+++ lib/current_earthquakes/api_service.rb
@@ -7,10 +7,10 @@ class ApiService
         url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson'
         response = RestClient.get(url)
         JSON.parse(response)["features"].each { |earthquake| Earthquakes.new(earthquake['properties'])}
-        binding.pry
+        #binding.pry
     
     end
+
 end
-#ApiService.get_earthquakes
-#e['features'][0]['properties']['mag']
+
 
--- lib/current_earthquakes/cli.rb
+++ lib/current_earthquakes/cli.rb
@@ -8,51 +8,35 @@ class CurrentEarthquakes::Cli
     def call
      ApiService.get_earthquakes
      greeting
-     get_date
      menu
     end
 
     def greeting
-       puts "Welcome to Earthquake News" 
-        colorizer = Lolize::Colorizer.new
-        #Colorizer.write(“Welcome to Earthquake News”)
-
+       puts "Welcome to Earthquake News!" 
+        puts "Hit enter to continue to the latest earthquakes."
     end
 
-    def get_earthquakes
-        place = gets.chomp
-        puts ApiService.get_earthquakes(place)
+    def event
+        Earthquakes.all.each.with_index { |earthquake| puts earthquake.place }
     end
 
-    # def get_date
-    #     puts "Please enter the date you want to review?"
-    #     date = gets.chomp
-    #     ApiService.get_earthquakes(date)
-    # end
     def menu
-        CurrentEarthquakes::Earthquake.all.get_last
-    #     colorizer = Lolize::Colorizer.new
-
-    #     prompt = TTY::Prompt.new(active_color: :cyan)
-    #         @menu = [
-    #             {"All events" => -> do list_all_events end},
-    #             {"Free events" => -> do free_events end},
-    #             {"Exit" => -> do goodbye end}
-    #         ]
+        prompt = TTY::Prompt.new(active_color: :cyan)
+             @menu = [
+                 {"All events" => ->  do 
+                 event 
+                
+                end},
+                 {"Exit" => -> do goodbye 
+                end}
+             ]
      
-    #         prompt.select("", @menu)
+            prompt.select("", @menu)
+            #binding.pry
     end
 
-
-    # def menu
-
-    #    prompt = TTY::Prompt.new
-    #    prompt.ask("EarthQuake date?, convert: :date")
-    #     #binding.pry
-
-    # end
-    def date()
-        date = gets.chomp
+    def goodbye
+        puts "Goodbye"
+        exit
     end
-
 end
