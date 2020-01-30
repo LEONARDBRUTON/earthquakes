8a96872ae7e8f44aa91d1adff860641072443d59
enigmalb <enigmalb@outlook.com>
2020-01-30 12:44:49 -0800

    some more visuals

--- lib/current_earthquakes/cli.rb
+++ lib/current_earthquakes/cli.rb
@@ -16,14 +16,7 @@ class CurrentEarthquakes::Cli
     end
 
     # def menu
-
-    #     puts "******************************************".colorize(:blue)
-    #     puts "**  See Earthquakes for the last 24hr?  **".colorize(:light_blue)
-    #     puts "******************************************".colorize(:blue)
-    #     puts "******************************************".colorize(:blue)
-    #     puts "**  Enter Y for Yes, or N for Exit?     **".colorize(:light_blue)
-    #     puts "******************************************".colorize(:blue)
-    #     puts "                                          "
+                                
         
     #     input = gets.strip.upcase
     #     if input == "Y"
@@ -44,8 +37,14 @@ class CurrentEarthquakes::Cli
     # end
 
     def menu
-     puts  "See Earthquakes for the last 24hr?".colorize(:light_blue)
-        puts "Enter Y for Yes or N for Exit.".colorize(:red)
+         puts "******************************************".colorize(:blue)
+         puts "**  See Earthquakes for the last 24hr?  **".colorize(:light_blue)
+         puts "******************************************".colorize(:blue)
+         puts "******************************************".colorize(:blue)
+         puts "**  Enter Y for Yes, or N for Exit?     **".colorize(:light_blue)
+         puts "******************************************".colorize(:blue)
+         puts "                                          "
+
         input = gets.strip.upcase
         if input == "Y"
          Earthquakes.all.each.with_index(1) do |earthquake, index|
@@ -82,32 +81,15 @@ class CurrentEarthquakes::Cli
         end
     end
 
-    # def list_choice
-    #     puts "Scroll & choose number of place,to see details, or N to exit?".colorize(:blue)
-    #     input = gets.strip
-    #     if input.upcase == "N"
-    #         goodbye
-    
-    #     elsif  input.to_i <= Earthquakes.all.count && input.to_i >= 1
-    #         earthquake = Earthquakes.all[input.to_i-1]
-    #         display(earthquake).colorize(:red)   
-    #    else
-    #         puts "**********************************".colorize(:red)
-    #         puts "**   INVALID INPUT, try again!  **".colorize(:red)
-    #         puts "**********************************".colorize(:red)
-    #         menu
-    #     end
-    # end
-
     def display(earthquake)
 
-       puts "*************************".colorize(:red)
+       puts "******************************".colorize(:red)
        puts earthquake.place
 
        puts earthquake.mag
 
        puts earthquake.time
-        puts "*************************".colorize(:red)
+       puts "******************************".colorize(:red)
         menu
     end
     
